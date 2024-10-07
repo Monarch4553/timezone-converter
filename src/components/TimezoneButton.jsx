bedc9d4b55dc58d59a6d582e1354d4344401b5b6
Monarch <jhondavidson1122@gmail.com>
2024-10-07 15:45:07 +0530

    asdasdas

--- src/components/TimezoneButton.jsx
+++ src/components/TimezoneButton.jsx
@@ -1,23 +1,15 @@
-import { Clock, Globe } from "lucide-react";
- 
-import { useEffect, useState } from "react";
+// app/components/TimezoneButton.jsx
+import { Globe } from "lucide-react";
 
+// Server component
 export default function TimezoneButton() {
-  const [timeZone, setTimeZone] = useState("");
-
-  useEffect(() => {
-    const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-    setTimeZone(userTimeZone);
-  }, []);
+  // Get the user's timezone on the server side
+  const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
   return (
- 
-
-  
-      <div className="px-4 items-center py-3 flex gap-2 bg-white   shadow-lg rounded-full absolute bottom-6 right-6">
-        <Globe strokeWidth={1.5} size={20} className="mt-[2px]" />
-        <p className="text-sm  ">{timeZone}</p>
-      </div>
-  
+    <div className="px-4 items-center py-3 flex gap-2 bg-white shadow-lg rounded-full absolute bottom-6 right-6">
+      <Globe strokeWidth={1.5} size={20} className="mt-[2px]" />
+      <p className="text-sm">{userTimeZone}</p>
+    </div>
   );
 }
