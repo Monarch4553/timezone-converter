b53b63ae02f5aed255c8221d57c4884996c48404
Monarch <jhondavidson1122@gmail.com>
2024-10-08 13:01:31 +0530

    changed font

--- src/app/[utcTimeString]/Container.js
+++ src/app/[utcTimeString]/Container.js
@@ -21,7 +21,7 @@ export default function Container({time}) {
 
     <div className="p-4 my-4 text-2xl justify-center items-center gap-6 rounded-2xl flex">
       <div className="flex flex-col text-center">
-        <h1 className="text-[4rem] sm:text-[8rem] leading-[1.2]">
+        <h1 className="text-[4rem] text-slate-600 font-medium sm:text-[8rem] leading-[1.2]">
           {localTimeFormatted.isValid()
             ? localTimeFormatted.format("HH")
             : "--"}
@@ -30,7 +30,7 @@ export default function Container({time}) {
       </div>
       <p className="text-[4rem] sm:text-[8rem] opacity-40 mb-14">:</p>
       <div className="flex flex-col text-center">
-        <h1 className="text-[4rem] sm:text-[8rem] leading-[1.2]">
+        <h1 className="text-[4rem] text-slate-600 font-medium sm:text-[8rem] leading-[1.2]">
           {localTimeFormatted.isValid()
             ? localTimeFormatted.format("mm")
             : "--"}
@@ -40,7 +40,7 @@ export default function Container({time}) {
     </div>
 
     <Link href="/">
-      <button className="inline-flex h-12 animate-shimmer items-center justify-center w-full rounded-md bg-[linear-gradient(110deg,white,45%,#f1f1f1,55%,white)] bg-[length:200%_100%] px-6 font-medium text-black transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+      <button className="inline-flex h-12 animate-shimmer items-center justify-center w-full rounded-md bg-[linear-gradient(110deg,white,45%,#d1eaff,55%,white)] bg-[length:200%_100%] px-6 font-medium text-black transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
         Generate your time
       </button>
     </Link>
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,10 @@
- 
-import localFont from "next/font/local";
+import { Poppins } from "next/font/google";
 import "./globals.css";
-import { HelmetProvider } from "react-helmet-async";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+// Import Poppins font
+const poppins = Poppins({
+  weight: ["100", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
 });
 
 // export const metadata = {
@@ -22,12 +15,8 @@ const geistMono = localFont({
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-    
-      {children}
-    
+      <body className={`${poppins.variable} antialiased`}>
+        {children}
       </body>
     </html>
   );
