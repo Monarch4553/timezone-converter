8641aaee343d890ba8a45620604c428c93cb5d8e
Monarch <jhondavidson1122@gmail.com>
2024-10-11 16:21:02 +0530

    changed url

--- src/app/[utcTimeString]/page.js
+++ src/app/[utcTimeString]/page.js
@@ -40,7 +40,7 @@ export default async function ViewRequestedTime({ params }) {
 
 // Function to fetch the decoded time
 async function decodeTime(encodedStr) {
-  const response = await fetch('http://localhost:3000/api/decode', {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/decode`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
